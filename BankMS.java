c2a0f950f1abc569e07d28f5881328a61fb32ac0
Code With Manish <96950041+code-with-manish@users.noreply.github.com>
2023-03-31 16:49:14 +0530

    Update BankMS.java

--- BankMS.java
+++ BankMS.java
@@ -13,14 +13,14 @@ class MBI{
     }
     public void depositCash(int amount){
         balance = balance + amount;
-        System.out.println(amount + " ₹ " + "Deposit successfully");
-        System.out.println("Current Balance : " + balance + "₹ ");
+        System.out.println(amount + " /- " + "Deposit successfully");
+        System.out.println("Current Balance : " + balance + "/- ");
     }
     public void withdrawCash(int amount){
         if (balance > amount ) {
             balance = balance - amount;
-            System.out.println(amount + "₹ Withdraw successfully");
-            System.out.println("Current Balance : " + balance + "₹ ");
+            System.out.println(amount + "/- Withdraw successfully");
+            System.out.println("Current Balance : " + balance + "/- ");
         }else {
             System.out.println("Withdrawal request failed! , insufficient balance");
         }
@@ -29,7 +29,7 @@ class MBI{
         System.out.println("Sure , Here your details");
         System.out.println("Account Name    : " + accountName);
         System.out.println("Account Number  : " + accountNum);
-        System.out.println("Account Balance : " + balance + "₹");
+        System.out.println("Account Balance : " + balance + "/-");
         System.out.println("Name Of Bank    : " + "Manish Bank Of India");
     }
 }
@@ -43,8 +43,8 @@ public class BankMS {
 
         System.out.print("Enter Account Number in 1-5 letters ");
         int accountNum = sc.nextInt();
-        if(accountNum <= 99999) {
-            System.out.println("Account successfully created\nYour Initial Balance is 500₹");
+        if (accountNum <= 99999) {
+            System.out.println("Account successfully created\nYour Initial Balance is 500/-");
             double accountBalance = 500;
 
             MBI bank = new MBI(accountNum, accountName, accountBalance);
@@ -53,35 +53,41 @@ public class BankMS {
                 System.out.println("1. Check Balance" + "  ,  2. Deposit Cash");
                 System.out.println("3. Withdraw Cash" + "  ,  4. Full Details");
                 System.out.println("5. Close Account");
-                int choose = sc.nextInt();
-
-                if (choose == 1) {
-                    bank.checkBal();
-                } else if (choose == 2) {
-                    System.out.println("Sure , Enter the amount");
-                    int amount = sc.nextInt();
-                    if (amount <= 100000) {
-                        bank.depositCash(amount);
-                    } else if (amount > 100001) {
-                        System.out.println("Deposit process failed!");
-                    } else {
-                        System.out.println("Deposit process failed!");
+                while (true) {
+                    try {
+                        int choose = sc.nextInt();
+                        if (choose == 1) {
+                            bank.checkBal();
+                        } else if (choose == 2) {
+                            System.out.println("Sure , Enter the amount");
+                            int amount = sc.nextInt();
+                            if (amount <= 100000) {
+                                bank.depositCash(amount);
+                            } else if (amount > 100001) {
+                                System.out.println("Deposit process failed!");
+                            } else {
+                                System.out.println("Deposit process failed!");
+                            }
+                        } else if (choose == 3) {
+                            System.out.println("Sure , Enter the amount");
+                            int amount = sc.nextInt();
+                            bank.withdrawCash(amount);
+                        } else if (choose == 4) {
+                            bank.details();
+                        } else if (choose == 5) {
+                            System.out.println("Successfully Closed.");
+                            System.exit(0);
+                        } else {
+                            System.out.println("Are you sure your number is in 1 - 5");
+                        }
+                    } catch (Exception MisMatch) {
+                        System.out.println("Are you sure you entered a number!");
+                        sc.next();
                     }
-                } else if (choose == 3) {
-                    System.out.println("Sure , Enter the amount");
-                    int amount = sc.nextInt();
-                    bank.withdrawCash(amount);
-                } else if (choose == 4) {
-                    bank.details();
-                } else if (choose == 5) {
-                    System.out.println("Successfully Closed.");
-                    System.exit(0);
-                } else {
-                    System.out.println("Are you sure your number is in 1 - 5");
                 }
             }
         } else {
-            System.out.println("Try Again!!");
+            System.out.println("Fill Another Form");
         }
     }
 }
